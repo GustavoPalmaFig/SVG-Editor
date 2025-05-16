f57f936d765527a0fd0ba805ac65df51186f753c
Gustavo F <Gustavopalmafigueroa@outlook.com>
2025-05-16 19:50:26 -0300

    🎉 initial: create project and add PrimeNG and Tailwind CSS configuration

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+  standalone: true,
+  imports: [ButtonModule],
+})
+export class AppComponent {}
