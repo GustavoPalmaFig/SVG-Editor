fce1d5baff2612e2a9897552cc6d7213d77528e5
Gustavo F <Gustavopalmafigueroa@outlook.com>
2025-05-19 15:03:16 -0300

    ✨ feat: enhance properties panel with prime ng color picker and input number components

--- src/app/core/shared/components/properties-panel/properties-panel.component.ts
+++ src/app/core/shared/components/properties-panel/properties-panel.component.ts
@@ -1,13 +1,24 @@
 import { FormsModule } from '@angular/forms';
+import { SliderModule } from 'primeng/slider';
 import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
+import { InputTextModule } from 'primeng/inputtext';
+import { ColorPickerModule } from 'primeng/colorpicker';
+import { InputNumberModule } from 'primeng/inputnumber';
 import { isRectangle, isStar } from '../../utils/type-guards';
 import { ElementService } from '../../services/element.service';
 
 @Component({
   selector: 'app-properties-panel',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    SliderModule,
+    InputNumberModule,
+    InputTextModule,
+    ColorPickerModule,
+  ],
   templateUrl: './properties-panel.component.html',
   styleUrls: ['./properties-panel.component.scss'],
 })
